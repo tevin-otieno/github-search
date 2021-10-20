19021a84f13df7acbb0abb015cf77070bca6c8f9
tevin-otieno <tevin.otieno@student.moringaschool.com>
2021-10-20 13:15:29 +0300

    added the repository class

---
+++ src/app/class/repository.ts
@@ -0,0 +1,8 @@
+export class Repository {
+    constructor(
+        public html_url:string,
+        public description:string,
+        public language:string,
+        public homepage:string
+    ){}
+}
