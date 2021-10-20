9e0e18945c0e9de74aa6e150ff3381902bed4cc7
tevin-otieno <tevin.otieno@student.moringaschool.com>
2021-10-20 13:11:43 +0300

    added the api keys and the base urls
    these are the basic files to make sure my app communicates with the github api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey:'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',
+  baseUrl :"https://api.github.com/users/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey:'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',
+  baseUrl :"https://api.github.com/users/"
 };
 
 /*
